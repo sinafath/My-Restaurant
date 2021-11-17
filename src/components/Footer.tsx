cef47ea3519d1f9282d43fcf63167d92fc295d90
sina <sinafathpoor@gmail.com>
2021-11-17 20:37:16 +0330

    style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 import styles from "../styles/Footer.module.css";
 const Footer = () => (
   <footer className={styles.footer} >
-    <h4> phone number: 843**632 </h4>
-    <h4> adress: 6*10 Washington St , CA 9*349, USA </h4>
+    <h4 className={styles.item}><span className={styles.title}> phone number:</span> 843*4632 </h4>
+    <h4 className={styles.item}> <span className={styles.title}>adress:</span> 6*10 Washington St , CA 923449, USA </h4>
   </footer>
 );
 export default Footer;
