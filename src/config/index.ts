5c836cf8a85e5010a918115da9aae70a9c5533b2
sinafath <83284138+sinafath@users.noreply.github.com>
2022-05-10 16:39:22 +0430

    Delete src/config directory

--- src/config/index.ts
+++
@@ -1,6 +0,0 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL // development and production
-  || 'http://localhost' //test
-export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL // development and production
-  || 'http://localhost' //test
